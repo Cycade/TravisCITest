d8a2d5ac4c2e054ecf58c3586175ab8a9ca40b1f
Cycade <neskii@126.com>
2016-07-17 15:59:43 +0800

    add PersonClass to Test

---
+++ src/main/java/Person.java
@@ -0,0 +1,14 @@
+/**
+ * Created by lichenqiang on 16/7/17.
+ */
+public class Person {
+    private int Height;
+
+    public int getHeight() {
+        return Height;
+    }
+
+    public void setHeight(int height) {
+        Height = height;
+    }
+}
---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lichenqiang on 16/7/17.
+ */
+public class PersonTest {
+
+    @Test
+    public void shouldGetHeight() throws Exception {
+        // given
+        Person bill = new Person();
+
+        // when
+        bill.setHeight(112);
+
+        // then
+        assertEquals(bill.getHeight(), 112);
+    }
+}
